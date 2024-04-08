295dd4a287b32fa0bdf010da8c096996e11d51c8
fcolecumberri <fcolecumberri@hotmail.com>
2024-04-08 01:51:38 +0000

    Made test.py a CLI
    Made test.py a Command Line Interface so people can use this from the command line with ease.

--- test.py
+++ test.py
@@ -1,31 +1,41 @@
-from PIL import Image
-import numpy as np
-import torch
-import tqdm
+import fire
 
-import deep_danbooru_model
+def scan_img(img_filename="test.jpg", threshold=0.5, stable_dif_print=False):
+    from PIL import Image
+    import numpy as np
+    import torch
+    import tqdm
 
-model = deep_danbooru_model.DeepDanbooruModel()
-model.load_state_dict(torch.load('model-resnet_custom_v3.pt'))
+    import deep_danbooru_model
 
-model.eval()
-model.half()
-model.cuda()
+    model = deep_danbooru_model.DeepDanbooruModel()
+    model.load_state_dict(torch.load('model-resnet_custom_v3.pt'))
 
-pic = Image.open("test.jpg").convert("RGB").resize((512, 512))
-a = np.expand_dims(np.array(pic, dtype=np.float32), 0) / 255
+    model.eval()
+    model.half()
+    model.cuda()
 
-with torch.no_grad(), torch.autocast("cuda"):
-    x = torch.from_numpy(a).cuda()
+    pic = Image.open(img_filename).convert("RGB").resize((512, 512))
+    a = np.expand_dims(np.array(pic, dtype=np.float32), 0) / 255
+    with torch.no_grad(), torch.autocast("cuda"):
+        x = torch.from_numpy(a).cuda()
+        # first run
+        y = model(x)[0].detach().cpu().numpy()
 
-    # first run
-    y = model(x)[0].detach().cpu().numpy()
+        # measure performance
+        for n in tqdm.tqdm(range(10)):
+            model(x)
 
-    # measure performance
-    for n in tqdm.tqdm(range(10)):
-        model(x)
+    output_array = [ (i, p) for i, p in enumerate(y) ]
+    output_array.sort(key=lambda x: x[1], reverse=True)
+
+    if stable_dif_print:
+        print( ", ".join([ f"({model.tags[i]}:p)" for i, p in output_array if p >= threshold ]) )
+    else:
+        for i, p in output_array:
+            if p >= threshold:
+                print(model.tags[i], p)
 
 
-for i, p in enumerate(y):
-    if p >= 0.5:
-        print(model.tags[i], p)
+if __name__ == '__main__':
+    fire.Fire(scan_img)
